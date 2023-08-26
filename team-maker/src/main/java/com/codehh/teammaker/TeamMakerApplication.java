d9f06f7591b66c0e2d0fbe3c53b9a1313155631a
노호준 <hh950131@gmail.com>
2023-08-26 14:06:12 +0900

    init

---
+++ team-maker/src/main/java/com/codehh/teammaker/TeamMakerApplication.java
@@ -0,0 +1,13 @@
+package com.codehh.teammaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMakerApplication.class, args);
+    }
+
+}
---
+++ team-maker/src/test/java/com/codehh/teammaker/TeamMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehh.teammaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
